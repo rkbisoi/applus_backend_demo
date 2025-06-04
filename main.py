00d54a3c0da19eea7b67f601129c5e7a8dc1ec17
rkbisoi <158269740+rkbisoi@users.noreply.github.com>
2025-06-05 03:05:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def validate_payment_simple(payment_data: PaymentValidationRequest) -> Dict[str,
         "valid": all_valid,
         # "validation_checks": validation_checks,
         # "security_checks": security_checks,
-        "amount_valid": 100.0 <= payment_data.amount <= 10000.0,  # Minimum amount
+        "amount_valid": 101.0 <= payment_data.amount <= 10000.0,  # Minimum amount
         "reference_valid": len(payment_data.reference_no) >= 6,  # Minimum reference length
         "bank_valid": payment_data.bank_name.strip() != "",
         "payment_type_valid": payment_data.payment_type in ["Bank In", "Online Transfer", "Credit Card"],
